f55cb964fd13c8bbf1b2a9e3381e39221aa624a8
shiba-tatsu <kusenohoanihimiro@gmail.com>
2020-08-21 21:45:30 +0900

    add item lists and display top image

---
+++ database/migrations/2020_08_21_035701_create_images_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('image');
+            $table->bigInteger('item_id')->unsigned();
+            $table->foreign('item_id')
+            ->references('id')->on('items')
+            ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('images');
+    }
+}
---
+++ resources/views/item/index.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-left">
+            @foreach ($items as $item)
+                <div class="col-md-4 mb-2">
+                    <div class="card">
+                        <div class="card-header">{{ $item->name }}</div>
+                        <div class="card-body">
+                            {{ $item->price }}
+                            {{ $item->images[0]->image }}
+                            <img src="{{ $item->images[0]->image }}">
+                            <img src="/public/images/crown-6916-bronze.png">
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+@endsection
