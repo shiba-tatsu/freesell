27da4ee20f694f278fe9970df8a64eb84940fc9a
shiba-tatsu <kusenohoanihimiro@gmail.com>
2021-01-05 11:42:54 +0900

    commit

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -9,31 +9,31 @@
   </div>
   <div class="row mb-5">
     <a class="col-4 p-2 text-center" href="{{route('payment.payment')}}">
-      <button type="button" class="userBtn ">クレジットカード登録</button>
+      <button type="button" class="userBtn btn-warning" style="width: 200px; height:50px; border:none;">クレジットカード登録</button>
     </a>
     <div class="col-4 p-2 text-center">
-      <button type="button" class="userBtn btn-warning">プロフィール編集</button>
+      <button type="button" class="userBtn btn-warning" style="width: 200px; height:50px; border:none;">プロフィール編集</button>
     </div>
     <div class="col-4 p-2 text-center">
-      <button type="button" class="userBtn btn-warning">商品一覧</button>
+      <button type="button" class="userBtn btn-warning" style="width: 200px; height:50px; border:none;">商品一覧</button>
     </div>
   </div>
 
   <div class="row mb-5">
     <div class="col-4 p-2 text-center">
-      <button type="button" class="userBtn btn-warning">いいね一覧</button>
+      <button type="button" class="userBtn btn-warning" style="width: 200px; height:50px; border:none;">いいね一覧</button>
     </div>
     <div class="col-4 p-2 text-center">
-      <button type="button" class="userBtn btn-warning">チャット</button>
+      <button type="button" class="userBtn btn-warning" style="width: 200px; height:50px; border:none;">チャット</button>
     </div>
     <div class="col-4 p-2 text-center">
-      <button type="button" class="userBtn btn-warning">購入履歴</button>
+      <button type="button" class="userBtn btn-warning" style="width: 200px; height:50px; border:none;">購入履歴</button>
     </div>
   </div>
 
   <form class="d-flex justify-content-center" method="POST" action="{{ route('logout') }}">
     @csrf
-    <button class="userBtn btn-warning" type="submit">
+    <button class="userBtn btn-warning" type="submit" style="width: 200px; height:50px; border:none;">
       ログアウト
     </button>
   </form>
